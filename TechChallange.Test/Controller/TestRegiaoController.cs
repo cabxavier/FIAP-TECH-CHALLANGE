9d0a2e361c15b5e11626dda3c354d8b0be4205b7
Christian-Massico <Chris.massicooliveira@outlook.com>
2024-09-24 19:41:22 -0300

    decimo segundo commit

---
+++ TechChallange.Test/Controller/TestRegiaoController.cs
@@ -0,0 +1,60 @@
+﻿using CORE.Repository;
+using CORE.Validator;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TechChallange.Test.MockData;
+using TECHCHALLANGEAPI.Controllers;
+using FluentAssertions;
+
+namespace TechChallange.Test.Controller
+{
+    public class TestRegiaoController
+    {
+        private RegiaoValidator regiaoValidador;
+
+        public TestRegiaoController()
+        {
+            this.regiaoValidador = new RegiaoValidator();
+        }
+
+        [Fact]
+        public async Task GetRegioesAsync_shouldReturn200Status()
+        {
+            var regiaoRepository = new Mock<IRegiaoRepository>();
+            regiaoRepository.Setup(_ => _.GetAllAsync()).ReturnsAsync(RegiaoMockData.GetRegiaos());
+
+            var sut = new RegiaoController(regiaoRepository.Object, this.regiaoValidador);
+
+            var result = (OkObjectResult)await sut.Get();
+            result.StatusCode.Should().Be(200);
+        }
+        [Fact]
+        public async Task GetRegioesAsync_shoulReturn204Status()
+        {
+            var regiaoRepository = new Mock<IRegiaoRepository>();
+            regiaoRepository.Setup(_ => _.GetAllAsync()).ReturnsAsync(RegiaoMockData.GetRegiaos());
+
+            var sut = new RegiaoController(regiaoRepository.Object,this.regiaoValidador);
+
+            var result = (NoContentResult)await sut.Get();
+            result.StatusCode.Should().Be(200);
+            regiaoRepository.Verify(_=>_.GetAllAsync(), Times.Exactly(1));
+
+        }
+
+        [Fact]
+
+        public async Task CreateRegiaoAsync_ShouldReturn201Status()
+        {
+            var regiaoRepository =new Mock<IRegiaoRepository>();
+            var regiaoValidador = new RegiaoValidator();
+            var regiaoInput = RegiaoMockData.GetRegiaos();
+            var sut = new RegiaoController (regiaoRepository.Object,this.regiaoValidador);
+
+            var result = (CreatedAtActionResult)await sut.Create(regiaoInput);
+            result.StatusCode.Should().Be(201);
+        }
+
+
+    }
+}
---
+++ TechChallange.Test/Repository/TestRegiaoRepository.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using INFRASTRUCTURE.Repository;
+using Microsoft.EntityFrameworkCore;
+using TechChallange.Test.MockData;
+
+
+namespace TechChallange.Test.Repository
+{
+    public class TestRegiaoRepository
+    {
+        protected readonly ApplicationDbContext _context;
+
+        public TestRegiaoRepository()
+        {
+            var option = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+            this._context= new ApplicationDbContext(option);
+            this._context.Database.EnsureCreated();
+        }
+        [Fact]
+        public async Task GetRegioesAsync_returnTarefacallection()
+        {
+            _context.Regiao.AddRange(RegiaoMockData.GetRegiaos());
+            _context.SaveChanges();
+
+            var sut = new RegiaoRepository(_context);
+
+            var result =await sut.GetAllAsync();
+            result.Should().HaveCount(RegiaoMockData.GetRegiaos().Count);
+         }
+
+        public void Dispose()
+        {
+            this._context.Database.EnsureDeleted();
+            this._context.Dispose   ( );
+        }
+
+    }
+}
