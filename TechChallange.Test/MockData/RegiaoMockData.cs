ba1f8e5a470afc5e80c07069d250dfc5bc8b67d4
Cesar Augusto Barbosa Xavier <cesaraugustobarbosaxavier@gmail.com>
2024-09-25 11:28:39 -0300

    decimo segundo commit

---
+++ TechChallange.Test/MockData/RegiaoMockData.cs
@@ -0,0 +1,38 @@
+﻿using CORE.Entity;
+
+namespace TechChallange.Test.MockData
+{
+    public class RegiaoMockData
+    {
+        public static List<Regiao> GetRegiaoAll()
+        {
+            return new List<Regiao>
+            {
+                new Regiao
+                {
+                    Id = 1,
+                    Ddd = "11",
+                    ContatosRegioes = new List<ContatoRegiao>()
+                },
+                new Regiao
+                {
+                    Id = 2,
+                    Ddd = "21",
+                    ContatosRegioes = new List<ContatoRegiao>()
+                },
+                new Regiao
+                {
+                    Id = 3,
+                    Ddd = "31",
+                    ContatosRegioes = new List<ContatoRegiao>()
+                },
+                new Regiao
+                {
+                    Id = 4,
+                    Ddd = "41",
+                    ContatosRegioes = new List<ContatoRegiao>()
+                }
+            };
+        }
+    }
+}
